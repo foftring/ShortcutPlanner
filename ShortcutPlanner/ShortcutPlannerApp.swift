92ee98f5062a6e401e31ce11b19f57e1ef99990b
Frank Oftring <frank.oftring@protonmail.com>
2022-12-03 18:07:29 -0500

    Initial Commit

---
+++ ShortcutPlanner/ShortcutPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutPlannerApp.swift
+//  ShortcutPlanner
+//
+//  Created by Frank Oftring on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
