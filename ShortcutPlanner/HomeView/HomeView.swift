b43c251a31ab35b5f839a289003f08cc5d72b5e7
Frank <frank.oftring@protonmail.com>
2022-12-19 20:33:20 -0500

    add navigationView to display card order view

--- ShortcutPlanner/HomeView/HomeView.swift
+++ ShortcutPlanner/HomeView/HomeView.swift
@@ -13,41 +13,52 @@ struct HomeView: View {
     @State var background: Color = .clear
     
     var body: some View {
+        NavigationView {
             ZStack {
-                background
-                ForEach(viewModel.cards) { card in
-                    CardView(offset: $viewModel.offset, card: card)
-                        .gesture(DragGesture()
-                            .onChanged { gesture in
-                                // Update the position of the top CardView
-                                viewModel.offset = gesture.translation
-                                if gesture.translation.width < -100 { background = .red }
-                                else if gesture.translation.width > 100 { background = .green }
-                                else { background = .clear }
-                                
-                            }
-                            .onEnded { gesture in
-                                if gesture.translation.width < -100 {
-                                    // Handle swipe left gesture
-                                    viewModel.removeCard(card: card)
-                                    print("Removing Card! -100")
-                                    viewModel.offset = .zero
-                                    background = .clear
-                                } else if gesture.translation.width > 100 {
-                                    // Handle swipe right gesture
-                                    viewModel.removeCard(card: card)
-                                    print("Removing Card! +100")
-                                    viewModel.offset = .zero
-                                    background = .clear
-                                    viewModel.runShortcut(card)
-                                } else {
-                                    viewModel.offset = .zero
-                                    background = .clear
+                    background
+                    ForEach(viewModel.cards) { card in
+                        CardView(offset: $viewModel.offset, card: card)
+                            .gesture(DragGesture()
+                                .onChanged { gesture in
+                                    // Update the position of the top CardView
+                                    viewModel.offset = gesture.translation
+                                    if gesture.translation.width < -100 { background = .red }
+                                    else if gesture.translation.width > 100 { background = .green }
+                                    else { background = .clear }
+                                    
                                 }
-                            }
-                        )
-                }
+                                .onEnded { gesture in
+                                    if gesture.translation.width < -100 {
+                                        // Handle swipe left gesture
+                                        viewModel.removeCard(card: card)
+                                        print("Removing Card! -100")
+                                        viewModel.offset = .zero
+                                        background = .clear
+                                    } else if gesture.translation.width > 100 {
+                                        // Handle swipe right gesture
+                                        viewModel.removeCard(card: card)
+                                        print("Removing Card! +100")
+                                        viewModel.offset = .zero
+                                        background = .clear
+                                        viewModel.runShortcut(card)
+                                    } else {
+                                        viewModel.offset = .zero
+                                        background = .clear
+                                    }
+                                }
+                            )
+                    }
+                    .navigationTitle("Today")
             }
+            .toolbar {
+                NavigationLink {
+                    Text("Change Card Order View!")
+                } label: {
+                    Image(systemName: "menucard.fill")
+                }
+
+            }
+        }
     }
 }
 
